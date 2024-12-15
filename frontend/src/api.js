347537f5bff0cb7d58a7f83e91cd7b165587b075
Chandrakant Pawar <137754266+pawarchandrakant29@users.noreply.github.com>
2024-12-16 04:45:25 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,7 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//     baseURL: "http://localhost:5000",
-// });
-
-// export default api;
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "freelancer-management-five.vercel.app",
+    baseURL: "http://localhost:5000",
 });
 
 export default api;
